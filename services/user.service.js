cae1751f9978913026adbc3428e83c22256aecba
Kevin Riquelme <kriquelme10@gmail.com>
2023-07-15 22:36:38 -0300

    tasks y no task

--- services/user.service.js
+++ services/user.service.js
@@ -12,7 +12,7 @@ class UserService {
 
   async find() {
     const client = await getConnection();
-    const rta = await client.query('SELECT * FROM task');
+    const rta = await client.query('SELECT * FROM tasks');
     return rta.rows;
   }
 
